844fab7a3d8a2c8256b2c672f7f8a91bfaa3e683
Emma <129emma@gmail.com>
2018-04-21 21:34:11 +1200

    assignment 8 for dynamic components and slot practice

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button @click="selectedColor = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+                <button @click="selectedColor = 'appGreen'" class="btn btn-success">Load Green Template</button>
+                <button @click="selectedColor = 'appRed'" class="btn btn-danger">Load Red Template</button>
+                <hr>
+             
+                {{ selectedColor }}
+                <keep-alive>
+                   <component :is="selectedColor">
+                   </component>    
+                </keep-alive>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        },
+        data: function (){
+            return {
+                title: 'This is what you click!',
+                selectedColor: ''
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/Blue.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p><slot>
+        </slot> </p>   
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid blue;
+        background-color: lightblue;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+</style>
